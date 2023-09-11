9bcf693c00f0bbab2ade80dd26db38fbc9efcb01
apubarman996 <apubarman782@gmail.com>
2023-09-11 19:44:15 +0900

    Change Designed

--- input.php
+++ input.php
@@ -6,21 +6,25 @@
     <title>Bootstrap Input</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   </head>
-  <body>
-    <h1>Input Hare</h1>
-    <form action="test.php" method="get">
-        <div class="mb-3">
-            <label for="formGroupExampleInput" class="form-label">Number One</label>
-            <input type="text" name="num1" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+  <body style="padding: 50px;"><br>
+    <div class="card" style="padding: 30px;">
+        <div class="card-body">
+            <h1>Input Hare</h1><hr>
+            <form action="test.php" method="get">
+                <div class="mb-3">
+                    <label for="formGroupExampleInput" class="form-label">Number One</label>
+                    <input type="text" name="num1" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+                </div>
+                <div class="mb-3">
+                    <label for="formGroupExampleInput2" class="form-label">Number Two</label>
+                    <input type="text"  name="num2" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Sum</button>
+                </div>
+            </form>
         </div>
-        <div class="mb-3">
-            <label for="formGroupExampleInput2" class="form-label">Number Two</label>
-            <input type="text"  name="num2" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
-        </div>
-        <div class="col-12">
-            <button type="submit" class="btn btn-primary">Sum</button>
-        </div>
-    </form>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   </body>
 </html>
